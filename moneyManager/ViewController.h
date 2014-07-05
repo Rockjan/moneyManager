c0ca58cf8a42a39f34b1f84c4d70321241ddc90c
ppnd <ppnd@ppnddeMacBook-Pro.local>
2014-07-05 20:51:31 +0000

    Initial Commit

---
+++ moneyManager/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  moneyManager
+//
+//  Created by ppnd on 14-7-5.
+//  Copyright (c) 2014年 zjut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ moneyManager/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  moneyManager
+//
+//  Created by ppnd on 14-7-5.
+//  Copyright (c) 2014年 zjut. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
